71ba1001e82e9feaac5a00a8e206126cd17d97d0
“Magd <majdgalyl@gmail.com>
2021-12-30 22:16:00 +0200

    Poiler 01

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PostList extends Component {
+  render() {
+    return (
+      <div>
+        Post Test
+      </div>
+    )
+  }
+}
+
+export default PostList;
