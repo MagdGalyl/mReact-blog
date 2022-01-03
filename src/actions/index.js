59b6ed5734e8595a775484e6a77539f3fc2c3571
“Magd <majdgalyl@gmail.com>
2022-01-03 02:04:15 +0200

    Async / Thunk Part Done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,22 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
-export const fetchPosts = async () => {
-  // Bad approach
+// export const fetchPosts = () => {
+//   return async (dispatch, getState) => {
+//     const response = await jsonPlaceHolder.get("/posts");
+
+//     dispatch({
+//       type: "FETCH_POSTS",
+//       payload: response,
+//     });
+//   };
+// };
+
+// Refactored Version
+export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceHolder.get("/posts");
 
-  return {
+  dispatch({
     type: "FETCH_POSTS",
-  };
+    payload: response,
+  });
 };
