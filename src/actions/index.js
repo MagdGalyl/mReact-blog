dd0e9f058511d16798bafe05755f83aae5742f9f
“Magd <majdgalyl@gmail.com>
2022-01-05 23:31:26 +0200

    Beta 5

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,22 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
-// export const fetchPosts = () => {
-//   return async (dispatch, getState) => {
-//     const response = await jsonPlaceHolder.get("/posts");
+export const fetchPosts = () => {
+  return async (dispatch, getState) => {
+    const response = await jsonPlaceHolder.get("/posts");
 
-//     dispatch({
-//       type: "FETCH_POSTS",
-//       payload: response,
-//     });
-//   };
-// };
+    dispatch({
+      type: "FETCH_POSTS",
+      payload: response,
+    });
+  };
+};
 
 // Refactored Version
-export const fetchPosts = () => async (dispatch) => {
-  const response = await jsonPlaceHolder.get("/posts");
+// export const fetchPosts = () => async (dispatch) => {
+//   const response = await jsonPlaceHolder.get("/posts");
 
-  dispatch({
-    type: "FETCH_POSTS",
-    payload: response,
-  });
-};
+//   dispatch({
+//     type: "FETCH_POSTS",
+//     payload: response,
+//   });
+// };
