7ba92d986dcb757d13c518b266b750d03eef501a
“Magd <majdgalyl@gmail.com>
2022-01-09 01:23:15 +0200

    Working on Multi-Request Solution

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
+import _ from "lodash";
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
+//---------------- FETCH POSTS-USER SOLUTION --------------- //
+
+export const fetchPostsAndUser = () => async (dispatch) => {
+  
+};
+
+//---------------- fetchPosts --------------- //
 export const fetchPosts = () => {
   return async (dispatch) => {
     const response = await jsonPlaceHolder.get("/posts");
@@ -20,3 +28,24 @@ export const fetchPosts = () => {
 //     payload: response,
 //   });
 // };
+
+//---------------- MEMOIZE FETCH USER SOLUTION --------------- //
+// export const fetchUser = (id) => async (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+
+// Extracting request to use lodash memoize to solve multi-request
+// by making helper function and memoize it since it will not work on action/action creator function
+// Downside to this ** WE can ONly fetch user data ONETIME only **
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceHolder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
+
+//---------------- ORIGINAL FETCH USER --------------- //
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceHolder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
